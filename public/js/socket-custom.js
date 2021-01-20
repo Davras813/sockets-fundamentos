3732554a77be50211f69e6025d2909beb27f4730
Davras <davidloaiza13@gmail.com>
2021-01-20 17:50:42 -0500

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,23 @@
+var socket = io();
+
+//Escuchar sucesos on
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+//Enviar información emit
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('Respuesta server', resp);
+});
+
+//Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor: ', mensaje);
+});
